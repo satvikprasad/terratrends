1480d983de11e821967798f7decd358f35ea0f2d
Satvik Prasad <satvikprasad@gmail.com>
2025-11-08 15:07:32 -0500

    Setup Shadcn and county clickables

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,119 @@
-import './App.css';
+import "./App.css";
 
-import { MapContainer, TileLayer } from 'react-leaflet'
+import { MapContainer, GeoJSON, useMap } from "react-leaflet";
+import type { GeoJsonObject, Feature } from "geojson";
 
-import { Menubar, MenubarMenu, MenubarContent, MenubarItem, MenubarSeparator, MenubarTrigger, MenubarShortcut } from '@/components/ui/menubar';
+import countyData from "./data/us_counties.json";
+
+import * as L from "leaflet";
+import { useCallback } from "react";
+import {
+    SidebarProvider,
+    Sidebar,
+    SidebarContent,
+    SidebarGroup,
+    SidebarGroupLabel,
+    SidebarGroupContent,
+    SidebarMenu,
+    SidebarMenuItem,
+    SidebarMenuButton,
+} from "./components/ui/sidebar";
+
+function RenderCounties(): React.JSX.Element {
+    const map = useMap();
+
+    const mouseOverCounty = useCallback((e: L.LeafletMouseEvent) => {
+        e.target.setStyle({
+            fillColor: "#ff6b6b",
+        });
+    }, []);
+
+    const mouseLeaveCounty = useCallback((e: L.LeafletMouseEvent) => {
+        e.target.setStyle(countyStyle);
+    }, []);
+
+    const onEachCounty: (feature: Feature, layer: L.Layer) => void = (
+        feature,
+        layer
+    ) => {
+        const name = feature.properties?.NAME;
+
+        if (!name) {
+            return;
+        }
+
+        const center = L.geoJSON(feature).getBounds().getCenter();
+
+        layer.on("mouseover", mouseOverCounty);
+        layer.on("mouseout", mouseLeaveCounty);
+
+        const marker = L.marker(center, {
+            icon: L.divIcon({
+                className: "county-label",
+                html: `<div class="county-label-inner">${name}</div>`,
+                iconSize: undefined,
+            }),
+        });
+
+        marker.addTo(map);
+    };
+
+    const countyStyle = {
+        fillColor: "#e6f3ff",
+        weight: 2,
+        opacity: 0.8,
+        color: "#ff3385",
+    };
+
+    return (
+        <GeoJSON
+            data={countyData as GeoJsonObject}
+            style={countyStyle}
+            onEachFeature={onEachCounty}
+        />
+    );
+}
 
 function App() {
     return (
         <>
-            <div className='absolute z-1000 left-1/2 -translate-x-1/2 top-6'>
-                <div className='relative'>
-                    <Menubar>
-                        <MenubarMenu>
-                            <MenubarTrigger>File</MenubarTrigger>
-                            <MenubarContent>
-                                <MenubarItem>
-                                    New Tab <MenubarShortcut>⌘T</MenubarShortcut>
-                                </MenubarItem>
-                                <MenubarItem>New Window</MenubarItem>
-                                <MenubarSeparator />
-                                <MenubarItem>Share</MenubarItem>
-                                <MenubarSeparator />
-                                <MenubarItem>Print</MenubarItem>
-                            </MenubarContent>
-                        </MenubarMenu>
-                    </Menubar>
+            <SidebarProvider>
+                <div className="flex flex-row">
+                    <Sidebar>
+                        <SidebarContent>
+                            <SidebarGroup>
+                                <SidebarGroupLabel>
+                                    TerraTrends
+                                </SidebarGroupLabel>
+                                <SidebarGroupContent>
+                                    <SidebarMenu>
+                                        <SidebarMenuItem>
+                                            <SidebarMenuButton>
+                                                Demo Button
+                                            </SidebarMenuButton>
+                                        </SidebarMenuItem>
+                                    </SidebarMenu>
+                                </SidebarGroupContent>
+                            </SidebarGroup>
+                        </SidebarContent>
+                    </Sidebar>
+                    <div className="min-h-screen">
+                        <MapContainer
+                            center={[33.275, -84.441]}
+                            zoom={8}
+                            scrollWheelZoom={false}
+                            style={{
+                                height: "100vh",
+                                width: "100vw",
+                            }}
+                        >
+                            <RenderCounties />
+                        </MapContainer>
+                    </div>
                 </div>
-            </div>
-            <div className='min-h-screen min-w-screen'>
-                <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} style={{
-                    height: '100vh',
-                    width: '100vw'
-                }}>
-                    <TileLayer
-                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                    />
-                </MapContainer>
-            </div>
+            </SidebarProvider>
         </>
-    )
+    );
 }
 
-export default App
+export default App;
